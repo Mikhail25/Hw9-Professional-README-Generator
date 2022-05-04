1bece0c2d8619a9052d978856dfb4b166042a849
Mikhail25 <42049923+Mikhail25@users.noreply.github.com>
2022-05-04 00:07:12 -0400

    complete commit

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your projects name?",
+                    "Please write a short description of your project?",
+                    "What kind of license should your project have?",
+                    "What command should be run to install dependencies?",
+                    "What command should be run to run tests?",
+                    "What does the user need to know about using the repo?",
+                    "What does the user need to know about contributing to the repo?"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+    const contents = generateMarkdown(data);
+
+    fs.appendFile(fileName+'.md', contents, (err) => {
+        err ? console.error(err) : console.log('Commit logged!');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: questions[0],
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: questions[1],
+
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: questions[2],                
+                choices: ['Apache', 
+                'BSD 3-Clause',
+                'BSD 2-Clause',
+                'MIT',
+                'Mozilla Public License 2.0',
+                'None'],
+            },
+            {
+                type: 'input',
+                name: 'commDepend',
+                message: questions[3],
+
+            },
+            {
+                type: 'input',
+                name: 'commTests',
+                message: questions[4],
+            },
+            {
+                type: 'input',
+                name: 'repoKnow',
+                message: questions[5],
+            },
+            {
+                type: 'input',
+                name: 'repoContribute',
+                message: questions[6],
+            },
+            
+        ])
+        .then((data) => {
+            writeToFile(data.projName, data);
+        });
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,130 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+  //[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  //![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)
+
+
+  switch(license){
+    case 'Apache' : 
+      return '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)'
+      break;
+    case 'BSD 3-Clause' :
+      return '![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)'
+      break;
+    case 'BSD 2-Clause' :
+      return '![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)'
+      break;
+    case 'MIT' :
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
+      break;
+    case 'Mozilla Public License 2.0' :
+      return '![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)'
+      break;
+    Default :
+      return "";
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+  //[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  //[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)
+
+  switch(license){
+    case 'Apache' : 
+      return `## License
+      
+      This project is license under the [Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)`
+      break;
+
+    case 'BSD 3-Clause' :
+      return `## License
+      
+      This project is license under the [BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause)`
+      break;
+
+    case 'BSD 2-Clause' :
+      return `## License
+      
+      This project is license under the [BSD 2-Clause License](https://opensource.org/licenses/BSD-2-Clause)`
+      break;
+
+    case 'MIT' :
+      return `## License
+      
+      This project is license under the [The MIT License](https://opensource.org/licenses/MIT)`
+      break;
+
+    case 'Mozilla Public License 2.0' :
+      return `## License
+      
+      This project is license under the [Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)`
+      break;
+    Default :
+      return "";
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license === 'None'){
+    return '';
+  }else{
+    return '* [License](#license)';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+
+  //functions to hold the rendering
+const badge = renderLicenseBadge(data.license);
+const licenseLink = renderLicenseLink(data.license);
+const licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
+  ${badge}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  *[Installation](#installation)
+  *[Usage](#usage)
+  ${licenseSection}
+  *[Contributing](#contributing)
+  *[Tests](#tests)
+
+
+  ## Installation
+
+  
+  > ${data.commDepend}
+
+  ## Usage 
+
+  ${data.repoKnow}
+
+  ${licenseLink}
+
+  ## Contributing
+
+  ${data.repoContribute}
+
+  ## Tests  
+
+  To run tests, run the following command: 
+
+  > ${data.commTests}
 
 `;
 }
